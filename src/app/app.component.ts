f80f3423ce13623d57471e649eb7b973df59b6ed
João da Mata <jlfilho@users.noreply.github.com>
2025-04-17 13:23:36 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app-olamundo';
+  title: string = 'Data Binding Application';
+  message: string = 'Olá mundo!';
+  imageUrl: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+  inputText: string = "";
+
+  showAlert() {
+    alert("Você clicou no botão!");
+  }
+
 }
